4efb8a3338975f8d399d7e42698f59945dfe2727
Edwar Roa <eroa1@udi.edu.co>
2021-06-18 19:50:48 -0500

    Update dependencia.php
    Se cambia el nombre de la clase

--- app/Models/dependencia.php
+++ app/Models/dependencia.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class dependencia extends Model
+class Dependencia extends Model
 {
     use HasFactory;
     protected $fillable = [
